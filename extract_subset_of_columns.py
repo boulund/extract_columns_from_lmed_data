7d8a1b800969fe244106ff43f7f2f23104b36f4d
Fredrik Boulund <fredrik.boulund@ki.se>
2017-05-02 16:28:17 +0200

    Added name main thing to make it importable into ipython. Also improved printout.

--- extract_subset_of_columns.py
+++ extract_subset_of_columns.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python3
 
 from sys import argv, exit
+import os
 import pandas as pd
 
-if len(argv) < 2:
-    print("Usage: script.py file.sas7bdat")
-    exit(1)
-else:
-    lmed_file = argv[1]
-    print("Using", lmed_file)
 
 def extract_columns_from_sas(filename, columns, output_csv, chunksize=100000):
     """Iterate through SAS file in chunks,
@@ -22,11 +17,22 @@ def extract_columns_from_sas(filename, columns, output_csv, chunksize=100000):
 
     for count, chunk in enumerate(reader, start=1):
         percent_read = count*chunksize / reader.row_count * 100
-        print("{:3.4f} % read".format(percent_read))
+        print("\r{:3.4f} % read".format(percent_read), end="")
         if count == 1:
             chunk.to_csv(output_csv, columns=columns, index=False, mode='a')
         else:
             chunk.to_csv(output_csv, columns=columns, index=False, mode='a', header=False)
 
 
-extract_columns_from_sas(lmed_file, columns=["lpnr", "KON", "atc", "EDATUM"], output_csv=lmed_file+".csv")
+
+if __name__ == "__main__":
+    if len(argv) < 2:
+        print("Usage: script.py file.sas7bdat")
+        exit(1)
+    else:
+        lmed_file = argv[1]
+        if os.path.isfile(lmed_file+".csv"):
+            print("WARNING:", lmed_file+".csv already exists!")
+            exit(2)
+
+    extract_columns_from_sas(lmed_file, columns=["lpnr", "KON", "atc", "EDATUM"], output_csv=lmed_file+".csv")
